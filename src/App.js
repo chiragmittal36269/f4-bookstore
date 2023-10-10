2bc4157764573aeed5a7dc51107746727d79cd17
Chirag Mittal <chiragmittal36269@gmail.com>
2023-10-10 17:03:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Clicked from "./Components/Clicked";
+import Find from "./Components/Find";
+import FirstPage from "./Components/FirstPage.js";
+import Navbar from "./Components/Navbar";
+import ShowData from "./Components/ShowData";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [clicked, setClicked] = useState("");
+    const [searchValue, setSearchValue] = useState("");
+    const [books, setBooks] = useState([]);
+    const [bookData, setBookData] = useState(null);
+
+    return (
+        <div>
+            <Navbar
+                setSearchValue={setSearchValue}
+                searchValue={searchValue}
+                setBooks={setBooks}
+            />
+            <Routes>
+                <Route
+                    path="/"
+                    element={<FirstPage setClicked={setClicked} />}
+                />
+                <Route
+                    path="/clicked"
+                    element={<Clicked value={{ clicked, setClicked }} />}
+                />
+                <Route
+                    path="/find"
+                    element={<Find value={books} setBookData={setBookData} />}
+                />
+
+                <Route
+                    path="/showData"
+                    element={<ShowData bookData={bookData} />}
+                />
+            </Routes>
+        </div>
+    );
+};
 
 export default App;
